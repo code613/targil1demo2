2e2542422721a8cefe8d284c81545d4706e1aceb
code613 <benmarcus724@gmail.com>
2018-03-27 14:44:10 +0300

    ok set up the first class of primitive which is the coordinate class

---
+++ src/Primitives/Coordinate.java
@@ -0,0 +1,42 @@
+package Primitives;
+//ok when thrue this class some Q's of it's end
+//ben marcus 305568867
+public class Coordinate implements Comparable<Coordinate>{//indeed how did we know to add this interface
+
+    //Fields
+
+    private double coordinate;//lower case c right class has to start with	a uppercase
+    // note the field is private
+
+    // ***************** Constructors ********************** //
+
+    public Coordinate()             { coordinate = 0.0;           }//empty
+    public Coordinate(double c)     { coordinate = c;             }//implement
+    public Coordinate(Coordinate c) { coordinate = c.coordinate;  }//copy
+
+    // ***************** Getters/Setters ********************** //
+
+    //note these are public methods  (method is in the class function is out - i think)
+    //wait no these are properties
+    public double getCoordinate()                {	return coordinate;	          }
+    public void setCoordinate(double c) { coordinate = c; }
+
+    // ***************** Administration  ******************** //
+
+    @Override//this means implementing the Comparable Interface (as interface's are virtual classes)
+    public int compareTo(Coordinate c) {//i guess this is the only function of the interface
+        return Double.compare(coordinate, c.coordinate);
+    }
+
+    // ***************** Operations ******************** //
+
+    // i don't know were this came from why you need it and what it means exactly
+    public void add (Coordinate c ){
+        coordinate += c.coordinate;
+    }
+
+    public void subtract(Coordinate c) {
+        coordinate -= c.coordinate;
+    }
+
+}
